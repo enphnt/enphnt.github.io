936265b78c0f4eb938e6677a5455a66784648cde
Nathan Phennel <nathanphennel@gmail.com>
2018-07-17 00:14:34 +1000

    feat: last fixups i can see for now

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,49 +3,50 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
 
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      var profilepic = "images/" + this.props.data.image;
       var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
+      var bio2 = this.props.data.bio2;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      var phone = this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
     }
 
     return (
       <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Nathan Phennel Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
+        <div className="row">
+          <div className="three columns">
+            <img className="profile-pic" src={profilepic} alt="Nathan Phennel Profile Pic" />
+          </div>
+          <div className="nine columns main-col">
             <h2>About Me</h2>
 
             <p>{bio}</p>
+            <p>{bio2}</p>
             <div className="row">
               <div className="columns contact-details">
                 <h2>Contact Details</h2>
-              <p className="address">
-                <span>{name}</span><br />
-                <span>{city} {state}, {zip}
-                </span><br />
-                <span>{email}</span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} download className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
+                <p className="address">
+                  <span>{name}</span><br />
+                  <span>{city} {state}, {zip}
+                  </span><br />
+                  <span>{email}</span>
+                </p>
+              </div>
+              <div className="columns download">
+                <p>
+                  <a href={resumeDownload} download className="button"><i className="fa fa-download"></i>Download Resume</a>
+                </p>
+              </div>
             </div>
-         </div>
-      </div>
+          </div>
+        </div>
 
-   </section>
+      </section>
     );
   }
 }
