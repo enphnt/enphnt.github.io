3dda3178c11e932e573e2e08edafa273c3589f97
Nathan Phennel <nathanphennel@gmail.com>
2018-07-17 18:22:38 +1000

    feat: grab current year for footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
+    var currentYear = (new Date()).getFullYear();
 
     if(this.props.data){
       var networks= this.props.data.social.map(function(network){
@@ -19,8 +20,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Nathan Phennel</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright {currentYear} Nathan Phennel</li>
            </ul>
 
         </div>
