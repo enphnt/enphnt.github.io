8e927fd032d690100b413e9f2d912657f594b6b6
Nathan Phennel <nathanphennel@gmail.com>
2018-07-17 19:33:08 +1000

    feat: border, title and better fonts for portfolio projects

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -3,19 +3,20 @@ import React, { Component } from 'react';
 class Portfolio extends Component {
   render() {
 
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function(projects) {
+        var projectImage = 'images/portfolio/' + projects.image;
         return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
+          <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
+              <img alt={projects.title} src={projectImage} />
+              <h5 className="portfolio-item-meta">{projects.title}</h5>
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
                 </div>
+              </div>
             </a>
           </div>
         </div>
@@ -24,19 +25,15 @@ class Portfolio extends Component {
 
     return (
       <section id="portfolio">
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
+        <div className="row">
+          <div className="twelve columns collapsed">
             <h1>Check Out Some of My Works.</h1>
-
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
+              {projects}
             </div>
           </div>
-      </div>
-   </section>
+        </div>
+      </section>
     );
   }
 }
