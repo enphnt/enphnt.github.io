b57927ef0ed64a5995b4fc23994a38167cac591f
Nathan Phennel <nathanphennel@gmail.com>
2018-07-17 18:22:07 +1000

    feat: better download button

--- src/Components/About.js
+++ src/Components/About.js
@@ -38,7 +38,7 @@ class About extends Component {
               </div>
               <div className="columns download">
                 <p>
-                  <a href={resumeDownload} download className="button"><i className="fa fa-download"></i>Download Resume</a>
+                  <a href={resumeDownload} download className="button"><i className="fa fa-download"></i>Download My Resume 🤗</a>
                 </p>
               </div>
             </div>
