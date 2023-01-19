ad8822fef8f69f86bb9c575eea18b505e1b6419e
Nathan Phennel <nathanphennel@gmail.com>
2023-01-19 14:19:41 +1100

    feat: reduce content and update resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -54,7 +54,7 @@ class Resume extends Component {
           </div>
         </div>
 
-        <div className="row skill">
+        {/* <div className="row skill">
 
           <div className="three columns header-col">
             <h1><span>Skills</span></h1>
@@ -71,7 +71,7 @@ class Resume extends Component {
               </ul>
             </div>
           </div>
-        </div>
+        </div> */}
       </section>
     );
   }
