dfd838835009a839b9e184fb3b69be0c7b92d23e
Nathan Phennel <nathanphennel@gmail.com>
2023-03-12 17:29:52 +1100

    fix: nav links now close navbox on click

--- src/Components/navbar/index.js
+++ src/Components/navbar/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { Link } from "gatsby";
 
-import NavbarLinks from "./navLinks";
 import Logo from "./logo";
 
 const Navigation = styled.nav`
@@ -73,15 +73,63 @@ const Hamburger = styled.div`
   }
   ::before {
     transform: ${props =>
-    props.open ? "rotate(-90deg) translate(-10px, 0px)" : "rotate(0deg)"};
+    props.open ? "rotate(-450deg) translate(-10px, 0px)" : "rotate(0deg)"};
     top: -10px;
   }
   ::after {
     opacity: ${props => (props.open ? "0" : "1")};
-    transform: ${props => (props.open ? "rotate(90deg) " : "rotate(0deg)")};
+    transform: ${props => (props.open ? "rotate(450deg) " : "rotate(0deg)")};
     top: 10px;
   }
 `;
+
+const NavItem = styled(Link)`
+  text-decoration: none;
+  color: #111;
+  display: inline-block;
+  white-space: nowrap;
+  margin: 0 1vw;
+  opacity: 1;
+  animation: fadeInOpacity 1s linear;
+  position: relative;
+  :after {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 0%;
+    content: ".";
+    color: transparent;
+    background: #4DA167;
+    height: 2px;
+    transition: all 0.4s ease-in;
+  }
+  :hover {
+    color: #35435d; 
+    ::after {
+      width: 100%;
+    }
+  }
+  @media (max-width: 768px) {
+    padding: 15px 0;
+    font-size: 2rem;
+    animation: none;
+    z-index: 6;
+    margin: 0 0 16px;
+    ::after {
+      height: 8px;
+    }
+  }
+  @keyframes fadeInOpacity {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
@@ -96,11 +144,21 @@ const Navbar = () => {
       </Toggle>
       {navbarOpen ? (
         <Navbox>
-          <NavbarLinks />
+          <NavItem onClick={() => setNavbarOpen(false)} to="/">Home</NavItem>
+          <NavItem onClick={() => setNavbarOpen(false)} to="/#about">About</NavItem>
+          <NavItem onClick={() => setNavbarOpen(false)} to="/#portfolio">Portfolio</NavItem>
+          <NavItem onClick={() => setNavbarOpen(false)} to="/#testimonials">Testimonials</NavItem>
+          <NavItem onClick={() => setNavbarOpen(false)} to="/blog">Blog</NavItem>
+          <NavItem onClick={() => setNavbarOpen(false)} to="/#contact">Contact</NavItem>
         </Navbox>
       ) : (
         <Navbox open>
-          <NavbarLinks />
+          <NavItem to="/">Home</NavItem>
+          <NavItem to="/#about">About</NavItem>
+          <NavItem to="/#portfolio">Portfolio</NavItem>
+          <NavItem to="/#testimonials">Testimonials</NavItem>
+          <NavItem to="/blog">Blog</NavItem>
+          <NavItem to="/#contact">Contact</NavItem>
         </Navbox>
       )}
     </Navigation>
