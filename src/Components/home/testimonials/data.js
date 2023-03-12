05f104acd82759f12c65210f5a1cf6f59891894d
Nathan Phennel <nathanphennel@gmail.com>
2023-03-12 11:43:31 +1100

    feat: adds testimonial photos from src

--- src/Components/home/testimonials/data.js
+++ src/Components/home/testimonials/data.js
@@ -1,37 +1,38 @@
+import raja from "./raja.jpg";
+import ken from "./ken.png";
+import jaya from "./jaya.png";
+import henry from "./henry.png";
+
 const reviews = [
   {
     id: 1,
     name: "Ken M.",
     job: "Technical Writer @Temenos",
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    image: ken,
     text:
       "Nathan operates with a long-term strategic view and a day-to-day sense of urgency. Not someone who sits back on his heels and waits for things to happen ... he sees the need and makes it happen now.",
   },
   {
     id: 2,
     name: "Raja Bellebon",
-    job: "Software Developer @Atlassian",
-    image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    job: "Software Dev @Atlassian",
+    image: raja,
     text:
       "I definitely recommend Nathan as he is a great agile tester with a strong appetite to learn. He is broad-minded and thinks outside of the box.",
   },
   {
     id: 3,
     name: "Henry Santoso",
-    job: "Senior Consultant @Servian",
-    image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    job: "Consultant @Servian",
+    image: henry,
     text:
       "Nate is a high-achiever who is capable of digesting any new advanced technical knowledge you present at him. A studious individual who will go the extra miles/hours.",
   },
   {
     id: 4,
     name: "Jaya Ramesh",
-    job: "Senior Programmer @Avoka",
-    image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    job: "Senior Dev @Viva Leisure",
+    image: jaya,
     text:
       "Nathan always aims to work towards achieving growth for the entire team, accomplishing goals, devising plans to achieve best results.",
   },
