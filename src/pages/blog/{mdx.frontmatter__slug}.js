8f80e2c2dcf795a65e768e13d1405f38d4b4401c
Nathan Phennel <nathanphennel@gmail.com>
2023-03-19 21:26:45 +1100

    chore: whitespace fixup for graphql

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -65,15 +65,15 @@ export const query = graphql`
   query($id: String) {
         mdx(id: {eq: $id}) {
         frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-      hero_image_alt
-      hero_image_credit_link
-      hero_image_credit_text
-      hero_image {
-        childImageSharp {
-        gatsbyImageData
-      }
+          title
+          date(formatString: "MMMM DD, YYYY")
+          hero_image_alt
+          hero_image_credit_link
+          hero_image_credit_text
+          hero_image {
+            childImageSharp {
+            gatsbyImageData
+          }
         }
       }
     }
