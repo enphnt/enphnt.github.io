2981d5c0646a9975add3a9bf8b06118882962e11
Nathan Phennel <nathanphennel@gmail.com>
2023-04-07 17:20:31 +1000

    lint: bypass jsx-a11y/media-has-caption rule

--- src/Components/audio.js
+++ src/Components/audio.js
@@ -4,6 +4,7 @@ const Audio = ({ src, text, ...props }) => (
   <div style={{ textAlign: "center" }}>
     <figure style={{ textAlign: "center", width: "100%" }} type="audio/mpeg">
       <figcaption>{text}</figcaption>
+      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
       <audio
         style={{ width: "inherit" }}
         controls="nodownload"
