9471a3879f0694951704a332c6d0f80be6eaa43f
Nathan Phennel <nathanphennel@gmail.com>
2023-04-10 17:25:48 +1000

    chore: prettify query

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -74,24 +74,24 @@ const BlogPost = ({ data, children }) => {
 };
 
 export const query = graphql`
-  query($id: String) {
-        mdx(id: {eq: $id}) {
-        tableOfContents
-        frontmatter {
-          title
-          tags
-          date(formatString: "MMMM DD, YYYY")
-          hero_image_alt
-          hero_image_credit_link
-          hero_image_credit_text
-          hero_image {
-            childImageSharp {
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      tableOfContents
+      frontmatter {
+        title
+        tags
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
             gatsbyImageData
           }
         }
       }
     }
   }
-      `;
+`;
 
 export default BlogPost;;
